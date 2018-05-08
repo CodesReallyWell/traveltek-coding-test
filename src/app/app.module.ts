572fadfcac3ffbd89b0276e62c0cbab2c431e6e1
Liam Newton <liamanewton@gmail.com>
2018-05-08 09:19:57 -0500

    fix eye count component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
@@ -17,8 +16,7 @@ import { EyeColorComponent } from './eye-color/eye-color.component';
   // telling angular we are using these
   imports: [
     BrowserModule,
-    HttpClientModule,   // include tool to communicate over http
-    NgbModule.forRoot() // for root of app
+    HttpClientModule   // include tool to communicate over http
   ],
   providers: [ PeopleService ],
   bootstrap: [AppComponent]
--- src/app/eye-color/eye-color.component.ts
+++ src/app/eye-color/eye-color.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { People } from '../model/people';
-import { Observable, of } from 'rxjs';
-import { count, flatMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { count, map } from 'rxjs/operators';
 
 
 @Component({
@@ -16,17 +16,17 @@ export class EyeColorComponent implements OnChanges, OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.greenEyes = 0;
+    this.countGreenEyes();
   }
 
   ngOnChanges() {
-
+    this.countGreenEyes();
   }
 
-  private countGreenEyes(){
+  private countGreenEyes() {
     if (this.people && this.people.length > 0) {
-      of(this.people)
-        .pipe(flatMap((person: People) => person.eyeColor))
+      of(...this.people)
+        .pipe(map((person: People) => person.eyeColor))
         .pipe(count(eyeColor => eyeColor === 'green'))
         .subscribe(greenEyes => this.greenEyes = greenEyes);
     }
